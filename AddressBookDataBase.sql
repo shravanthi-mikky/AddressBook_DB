09ff7b747388d1aa61a26b4341baa86cddf07e66
shravanthi-mikky <shravanthi27041996@gmail.com>
2022-06-06 12:27:33 +0530

    [shravanthi] -Insert Contacts in AddressBook table

--- AddressBookDataBase.sql
+++ AddressBookDataBase.sql
@@ -14,3 +14,12 @@ Zip int,
 PhoneNumber Varchar(100),
 Email_ID Varchar(100)
 );
+
+----------uc3-Insert new Contacts to Address Book----------------
+
+Insert into AddressBook Values ('Shravanthi','Pabboji','ABC Colony','Old Alwal','Telangana',500010,'9000000001','shravanthi@gmail.com');
+Insert into AddressBook Values ('Vishnu','Pabboji','ABC Colony','Old Alwal','Telangana',500010,'9000000002','Vishnu@gmail.com');
+Insert into AddressBook Values ('Bhagya','Mallapuram','ABC Colony','Temple Alwal','Telangana',500010,'9000000003','Bhagya@gmail.com');
+Insert into AddressBook Values ('Santosh','Mallapuram','ABC Colony','Temple Alwal','Telangana',500010,'9000000004','Santoo@gmail.com');
+Insert into AddressBook Values ('Vihaan','Mallapuram','ABC Colony','Temple Alwal','Telangana',500010,'9000000005','Tippu@gmail.com');
+Select * from AddressBook;
